9b44a7175d73fc3c1153d9734b7ed95698e02a81
surajgoraicse <surajgoraicse@gmail.com>
2025-08-03 18:13:21 +0530

    blog list gap fix

--- src/app/blogs/components/BlogList.tsx
+++ src/app/blogs/components/BlogList.tsx
@@ -39,7 +39,7 @@ async function BlogList() {
 	const blogs = await getBlogs();
 
 	return (
-		<div className="w-full px-64 max-[1025px]:px-0 max-[1285px]:px-0 max-sm:px-2 flex flex-col gap-6 items-center mt-4 pb-8 max-sm:overflow-hidden">
+		<div className="w-full px-64 max-[1025px]:px-0 max-[1285px]:px-0 max-sm:px-2 flex flex-col items-center mt-4 pb-8 max-sm:overflow-hidden ">
 			{blogs.map((blog: IBlog, idx: number) => (
 				<BlogCard
 					key={idx}
