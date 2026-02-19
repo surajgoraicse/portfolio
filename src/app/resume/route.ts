a8560d36a920645fbc42c4411e723029fb386ff6
surajgoraicse <surajgoraicse@gmail.com>
2026-02-19 20:24:36 +0530

    resume route

--- src/app/resume/route.ts
+++ src/app/resume/route.ts
@@ -1,5 +1,15 @@
+import fs from "fs/promises";
+import path from "path";
 
+export async function GET() {
+	// Example: read a file from the local filesystem
+	const filePath = path.join(process.cwd(), "public/suraj.pdf");
+	const buffer = await fs.readFile(filePath);
 
+	// Set headers to trigger a download
+	const headers = new Headers();
+	headers.append("Content-Disposition", 'attachment; filename="suraj.pdf"');
+	headers.append("Content-Type", "application/pdf");
 
-
-
+	return new Response(buffer, { headers });
+}
